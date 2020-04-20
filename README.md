d1787834d2f44d6ce6e4e71779e7da4d8f5d0723
Hax4Us <lkpandey950@gmail.com>
2020-04-20 11:55:11 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Repository for new/unstable packages
---
+++ scripts/package_uploader.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+##
+##  Package uploader
+##
+##  Lokesh Pandey @Hax4Us <lkpandey950@gmail.com>
+##
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+set -o nounset
+
+# Check dependencies.
+if [ -z "$(command -v curl)" ]; then
+    echo "[!] Package 'curl' is not installed."
+    exit 1
+fi
+
+echo -n "[*] Uploading packages... "
+if RESULT_URL=$(curl --silent -F "${1}" https://file.io) > /dev/null 2>&1; then
+    echo "ok"
+    echo
+    echo "Packages uploaded to ${RESULT_URL}"
+    exit 0
+else
+    echo "[!] Failed to upload packages."
+    echo
+    echo "However, since build was successfull, exiting"
+    exit 0
+fi
